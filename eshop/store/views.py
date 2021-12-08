7890010e0ca6c281b7d7c3297eff603ea7b09eb3
Sicret89 <kamil.banas787@gmail.com>
2021-12-08 06:16:37 +0000

    0 cart values for unregister user that I fix later

--- eshop/store/views.py
+++ eshop/store/views.py
@@ -16,7 +16,8 @@ def cart(request):
         items = order.orderitem_set.all()
     else:
         items = []
-
+        order = {"get_cart_total": 0, "get_cart_items": 0}
+    # to be changed later for unregister user.
     context = {"items": items, "order": order}
     return render(request, "store/cart.html", context)
 
